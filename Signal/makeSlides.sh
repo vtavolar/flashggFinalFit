ae6b52508eb4938108859e71aecdf4a7c3fd31ba
vtavolar <vittorio.tavolaro@cern.ch>
2016-06-09 13:45:54 +0200

    make slides excluding wh,zh

--- Signal/makeSlides.sh
+++ Signal/makeSlides.sh
@@ -15,16 +15,16 @@ while read p
 do
 echo "Making slide for $p"
 cp $pwd0/tex/slide.tex $pwd0/$EXT/sigfit/slides/slide_${p}.tex
-sed -i -e "s/!NAMEEXT!/$EXT/g" $pwd0/$EXT/sigfit/slides/slide_${p}.tex
-sed -i -e "s/!NAME!/$p/g" $pwd0/$EXT/sigfit/slides/slide_${p}.tex
+sed -i  "s/!NAMEEXT!/$EXT/g" $pwd0/$EXT/sigfit/slides/slide_${p}.tex
+sed -i  "s/!NAME!/$p/g" $pwd0/$EXT/sigfit/slides/slide_${p}.tex
 q=${p/_/ }
 r=${q/_/ }
-sed -i -e "s/!TITLE!/$r/g" $pwd0/$EXT/sigfit/slides/slide_${p}.tex
+sed -i  "s/!TITLE!/$r/g" $pwd0/$EXT/sigfit/slides/slide_${p}.tex
 
 done < tmp.txt
 
-echo "cat  $pwd0/$EXT/sigfit/slides/Intro.tex $pwd0/$EXT/sigfit/slides/slide*{ggh,vbf,tth,wh,zh}*tex  $pwd0/$EXT/sigfit/slides/Outro.tex > $pwd0/$EXT/sigfit/slides/fullslides.tex"
-cat  $pwd0/$EXT/sigfit/slides/Intro.tex $pwd0/$EXT/sigfit/slides/slide*{ggh,vbf,tth,wh,zh}*tex  $pwd0/$EXT/sigfit/slides/Outro.tex > $pwd0/$EXT/sigfit/slides/fullslides.tex
+echo "cat  $pwd0/$EXT/sigfit/slides/Intro.tex $pwd0/$EXT/sigfit/slides/slide*{ggh,vbf,tth}*tex  $pwd0/$EXT/sigfit/slides/Outro.tex > $pwd0/$EXT/sigfit/slides/fullslides.tex"
+cat  $pwd0/$EXT/sigfit/slides/Intro.tex $pwd0/$EXT/sigfit/slides/slide*{ggh,vbf,tth}*tex  $pwd0/$EXT/sigfit/slides/Outro.tex > $pwd0/$EXT/sigfit/slides/fullslides.tex
 
 
 cd -
