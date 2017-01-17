be2b554620060259e34d5a20adecb88b2844a75d
vtavolar <vittorio.tavolaro@cern.ch>
2017-01-17 17:03:14 +0100

    again mostly indentation

--- Signal/test/makeParametricSignalModelPlots.cpp
+++ Signal/test/makeParametricSignalModelPlots.cpp
@@ -116,7 +116,7 @@ map<string,RooDataSet*> getFlashggData(RooWorkspace *work, int ncats, int m_hyp)
 	for (int cat=0; cat<ncats; cat++){
 		result.insert(pair<string,RooDataSet*>(Form("%s",flashggCats_[cat].c_str()),(RooDataSet*)work->data(Form("sig_mass_m%3d_%s",m_hyp,flashggCats_[cat].c_str()))));
 	}
-	result.insert(pair<string,RooDataSet*>("all",(RooDataSet*)work->data(Form("sig_mass_m%3d_AllCats",m_hyp))));
+	//	result.insert(pair<string,RooDataSet*>("all",(RooDataSet*)work->data(Form("sig_mass_m%3d_AllCats",m_hyp))));
 
 	return result;
 }
@@ -126,11 +126,13 @@ map<string,RooDataSet*> getFlashggDataGranular(RooWorkspace *work, int ncats, in
 	map<string,RooDataSet*> result;
 
 	for (int cat=0; cat<ncats; cat++){
-    for (int proc=0; proc < procs_.size() ; proc++){
-		result.insert(pair<string,RooDataSet*>(Form("%s_%s",procs_[proc].c_str(),flashggCats_[cat].c_str()),(RooDataSet*)work->data(Form("sig_%s_mass_m%3d_%s",procs_[proc].c_str(),m_hyp,flashggCats_[cat].c_str()))));
-    assert(work->data(Form("sig_%s_mass_m%3d_%s",procs_[proc].c_str(),m_hyp,flashggCats_[cat].c_str())));
+	  for (int proc=0; proc < procs_.size() ; proc++){
+//	    std::cout<<"cat "<<cat<<std::endl;
+//	    std::cout<<"proc "<<proc<<std::endl;
+	    result.insert(pair<string,RooDataSet*>(Form("%s_%s",procs_[proc].c_str(),flashggCats_[cat].c_str()),(RooDataSet*)work->data(Form("sig_%s_mass_m%3d_%s",procs_[proc].c_str(),m_hyp,flashggCats_[cat].c_str()))));
+	    assert(work->data(Form("sig_%s_mass_m%3d_%s",procs_[proc].c_str(),m_hyp,flashggCats_[cat].c_str())));
 	  }
-  }
+	}
 
 	return result;
 }
@@ -152,7 +154,9 @@ map<string,RooAddPdf*> getFlashggPdfs(RooWorkspace *work, int ncats){
 	for (int cat=0; cat<ncats; cat++){
 		result.insert(pair<string,RooAddPdf*>(Form("%s",flashggCats_[cat].c_str()),(RooAddPdf*)work->pdf(Form("sigpdfrel%s_allProcs",flashggCats_[cat].c_str()))));
 	}
-	result.insert(pair<string,RooAddPdf*>("all",(RooAddPdf*)work->pdf("sigpdfrelAllCats_allProcs")));
+	std::cout<<work->pdf("sigpdfrelAllCats_allProcs")<<std::endl;
+	std::cout<<work->pdf("sigpdfrelAllCats_allProcs")->GetName()<<std::endl;
+	//	result.insert(pair<string,RooAddPdf*>("all",(RooAddPdf*)work->pdf("sigpdfrelAllCats_allProcs")));
 
 	return result;
 }
@@ -192,7 +196,9 @@ void printInfo(map<string,RooDataSet*> data, map<string,RooAddPdf*> pdfs){
 }
 
 pair<double,double> getEffSigma(RooRealVar *mass, RooAbsPdf *pdf, double wmin=110., double wmax=130., double step=0.002, double epsilon=1.e-4){
-
+  cout<<"Entering getEffSigma"<<endl;
+  cout<<pdf->GetName()<<endl;
+  //  pdf->Print("V");
 	RooAbsReal *cdf = pdf->createCdf(RooArgList(*mass));
 	cout << "Computing effSigma...." << endl;
 	TStopwatch sw;
@@ -378,63 +384,63 @@ void performClosure(RooRealVar *mass, RooAbsPdf *pdf, RooDataSet *data, string c
 
 void Plot(RooRealVar *mass, RooDataSet *data, RooAbsPdf *pdf, pair<double,double> sigRange, vector<double> fwhmRange, string title, string savename){
 
-	double semin=sigRange.first;
-	double semax=sigRange.second;
-	double fwmin=fwhmRange[0];
-	double fwmax=fwhmRange[1];
-	double halfmax=fwhmRange[2];
-	double binwidth=fwhmRange[3];
+  double semin=sigRange.first;
+  double semax=sigRange.second;
+  double fwmin=fwhmRange[0];
+  double fwmax=fwhmRange[1];
+  double halfmax=fwhmRange[2];
+  double binwidth=fwhmRange[3];
   vector<double> negWeightBins;
   vector<double> negWeightBinsValues;
-	RooPlot *plot = mass->frame(Bins(binning_),Range("higgsRange"));
+  RooPlot *plot = mass->frame(Bins(binning_),Range("higgsRange"));
   plot->SetMinimum(0.0);
   if (markNegativeBins_){
-  TH1F *rdh = (TH1F*) data->createHistogram("CMS_hgg_mass",*mass,Binning(binning_,105,140));
-  for(unsigned int iBin =0 ; iBin < rdh->GetNbinsX() ; iBin++){
-  float content = rdh->GetBinContent(iBin);
-  float center = rdh->GetBinCenter(iBin);
-  if(content <0) {
-  std::cout <<" BIN "<< iBin << " has negative weight : " << content << " at " << center << std::endl;
-   negWeightBins.push_back(center);
-   negWeightBinsValues.push_back(content);
-  }
-  }
+    TH1F *rdh = (TH1F*) data->createHistogram("CMS_hgg_mass",*mass,Binning(binning_,105,140));
+    for(unsigned int iBin =0 ; iBin < rdh->GetNbinsX() ; iBin++){
+      float content = rdh->GetBinContent(iBin);
+      float center = rdh->GetBinCenter(iBin);
+      if(content <0) {
+	std::cout <<" BIN "<< iBin << " has negative weight : " << content << " at " << center << std::endl;
+	negWeightBins.push_back(center);
+	negWeightBinsValues.push_back(content);
+      }
+    }
   }
   double offset =0.05;
-	if (data) data->plotOn(plot,Invisible());
-	pdf->plotOn(plot,NormRange("higgsRange"),Range(semin,semax),FillColor(19),DrawOption("F"),LineWidth(2),FillStyle(1001),VLines(),LineColor(15));
-	TObject *seffLeg = plot->getObject(int(plot->numItems()-1));
-	pdf->plotOn(plot,NormRange("higgsRange"),Range(semin,semax),LineColor(15),LineWidth(2),FillStyle(1001),VLines());
-	pdf->plotOn(plot,NormRange("higgsRange"),Range("higgsRange"),LineColor(kBlue),LineWidth(2),FillStyle(0));
-	TObject *pdfLeg = plot->getObject(int(plot->numItems()-1));
-	if (data) data->plotOn(plot,MarkerStyle(kOpenSquare));
-	TObject *dataLeg = plot->getObject(int(plot->numItems()-1));
-	//TLegend *leg = new TLegend(0.15,0.89,0.5,0.55);
-	TLegend *leg = new TLegend(0.15+offset,0.40,0.5+offset,0.82);
-	leg->SetFillStyle(0);
-	leg->SetLineColor(0);
-	leg->SetTextSize(0.037);
-	if (data) leg->AddEntry(dataLeg,"#bf{Simulation}","lep");
-	leg->AddEntry(pdfLeg,"#splitline{#bf{Parametric}}{#bf{model}}","l");
-	leg->AddEntry(seffLeg,Form("#bf{#sigma_{eff} = %1.2f GeV}",0.5*(semax-semin)),"fl");
+  if (data) data->plotOn(plot,Invisible());
+  pdf->plotOn(plot,NormRange("higgsRange"),Range(semin,semax),FillColor(19),DrawOption("F"),LineWidth(2),FillStyle(1001),VLines(),LineColor(15));
+  TObject *seffLeg = plot->getObject(int(plot->numItems()-1));
+  pdf->plotOn(plot,NormRange("higgsRange"),Range(semin,semax),LineColor(15),LineWidth(2),FillStyle(1001),VLines());
+  pdf->plotOn(plot,NormRange("higgsRange"),Range("higgsRange"),LineColor(kBlue),LineWidth(2),FillStyle(0));
+  TObject *pdfLeg = plot->getObject(int(plot->numItems()-1));
+  if (data) data->plotOn(plot,MarkerStyle(kOpenSquare));
+  TObject *dataLeg = plot->getObject(int(plot->numItems()-1));
+  //TLegend *leg = new TLegend(0.15,0.89,0.5,0.55);
+  TLegend *leg = new TLegend(0.15+offset,0.40,0.5+offset,0.82);
+  leg->SetFillStyle(0);
+  leg->SetLineColor(0);
+  leg->SetTextSize(0.037);
+  if (data) leg->AddEntry(dataLeg,"#bf{Simulation}","lep");
+  leg->AddEntry(pdfLeg,"#splitline{#bf{Parametric}}{#bf{model}}","l");
+  leg->AddEntry(seffLeg,Form("#bf{#sigma_{eff} = %1.2f GeV}",0.5*(semax-semin)),"fl");
   
-	plot->GetXaxis()->SetNdivisions(509);
-	halfmax*=(plot->getFitRangeBinW()/binwidth);
-	TArrow *fwhmArrow = new TArrow(fwmin,halfmax,fwmax,halfmax,0.02,"<>");
-	fwhmArrow->SetLineWidth(2.);
-	TPaveText *fwhmText = new TPaveText(0.17+offset,0.3,0.45+offset,0.40,"brNDC");
-	fwhmText->SetFillColor(0);
-	fwhmText->SetLineColor(kWhite);
-	fwhmText->SetTextSize(0.037);
-	fwhmText->AddText(Form("FWHM = %1.2f GeV",(fwmax-fwmin)));
+  plot->GetXaxis()->SetNdivisions(509);
+  halfmax*=(plot->getFitRangeBinW()/binwidth);
+  TArrow *fwhmArrow = new TArrow(fwmin,halfmax,fwmax,halfmax,0.02,"<>");
+  fwhmArrow->SetLineWidth(2.);
+  TPaveText *fwhmText = new TPaveText(0.17+offset,0.3,0.45+offset,0.40,"brNDC");
+  fwhmText->SetFillColor(0);
+  fwhmText->SetLineColor(kWhite);
+  fwhmText->SetTextSize(0.037);
+  fwhmText->AddText(Form("FWHM = %1.2f GeV",(fwmax-fwmin)));
   std::cout << " [FOR TABLE] Tag " << data->GetName() << "=, Mass " << mass->getVal() << " sigmaEff=" << 0.5*(semax-semin) << "= , FWMH=" << (fwmax-fwmin)/2.35 << "=" << std::endl;
   std::cout << " [RESOLUTION CHECK] Ta/Procg " << data->GetName() << ", Mass " << mass->getVal() << " sigmaEff=" << 0.5*(semax-semin) << " , FWMH=" << (fwmax-fwmin)/2.35 << "" << std::endl;
-
-	//TLatex lat1(0.65,0.85,"#splitline{CMS Simulation}{}");
+  
+  //TLatex lat1(0.65,0.85,"#splitline{CMS Simulation}{}");
   TLatex  lat1(.129+0.03+offset,0.85,"H#rightarrow#gamma#gamma");
-	lat1.SetNDC(1);
-	lat1.SetTextSize(0.047);
-
+  lat1.SetNDC(1);
+  lat1.SetTextSize(0.047);
+  
   TString catLabel_humanReadable  = title;
   catLabel_humanReadable.ReplaceAll("_"," ");
   catLabel_humanReadable.ReplaceAll("UntaggedTag","Untagged");
@@ -442,46 +448,46 @@ void Plot(RooRealVar *mass, RooDataSet *data, RooAbsPdf *pdf, pair<double,double
   catLabel_humanReadable.ReplaceAll("TTHLeptonicTag","TTH Leptonic Tag");
   catLabel_humanReadable.ReplaceAll("TTHHadronicTag","TTH Hadronic Tag");
   catLabel_humanReadable.ReplaceAll("all","All Categories");
-
-	TLatex lat2(0.93,0.88,catLabel_humanReadable);
+  
+  TLatex lat2(0.93,0.88,catLabel_humanReadable);
   lat2.SetTextAlign(33);
-	lat2.SetNDC(1);
-	lat2.SetTextSize(0.045);
-
-	TCanvas *canv = new TCanvas("canv","canv",650,600);
+  lat2.SetNDC(1);
+  lat2.SetTextSize(0.045);
+  
+  TCanvas *canv = new TCanvas("canv","canv",650,600);
   canv->SetLeftMargin(0.16);
   canv->SetTickx(); canv->SetTicky();
-	plot->SetTitle("");
-	plot->GetXaxis()->SetTitle("m_{#gamma#gamma} (GeV)");
-	plot->GetXaxis()->SetTitleSize(0.05);
-	plot->GetYaxis()->SetTitleSize(0.05);
-	plot->GetYaxis()->SetTitleOffset(1.5);
+  plot->SetTitle("");
+  plot->GetXaxis()->SetTitle("m_{#gamma#gamma} (GeV)");
+  plot->GetXaxis()->SetTitleSize(0.05);
+  plot->GetYaxis()->SetTitleSize(0.05);
+  plot->GetYaxis()->SetTitleOffset(1.5);
   plot->SetMinimum(0.0);
-	plot->Draw();
-	fwhmArrow->Draw("same <>");
-	fwhmText->Draw("same");
- 	//lat1.Draw("same");
-	lat2.Draw("same");
+  plot->Draw();
+  fwhmArrow->Draw("same <>");
+  fwhmText->Draw("same");
+  //lat1.Draw("same");
+  lat2.Draw("same");
   lat1.Draw("same");
-	leg->Draw("same");
+  leg->Draw("same");
   for (unsigned int i =0 ; i < negWeightBins.size() ; i++){
-	
-  TArrow *negBinsArrow = new TArrow(negWeightBins[i],0.0,negWeightBins[i],halfmax/2,0.02,"<>");
-	negBinsArrow->SetLineWidth(2.);
-	negBinsArrow->SetLineColor(kRed);
-  
-  negBinsArrow->Draw("same <>");
-
+    
+    TArrow *negBinsArrow = new TArrow(negWeightBins[i],0.0,negWeightBins[i],halfmax/2,0.02,"<>");
+    negBinsArrow->SetLineWidth(2.);
+    negBinsArrow->SetLineColor(kRed);
+    
+    negBinsArrow->Draw("same <>");
+    
   }
   string sim="Simulation Preliminary";
   CMS_lumi( canv, 0,0,sim);
-	canv->Print(Form("%s.pdf",savename.c_str()));
-	canv->Print(Form("%s.png",savename.c_str()));
-	//string path = savename.substr(0,savename.find('/'));
-	//canv->Print(Form("%s/animation.gif+100",path.c_str()));
-
-	delete canv;
-
+  canv->Print(Form("%s.pdf",savename.c_str()));
+  canv->Print(Form("%s.png",savename.c_str()));
+  //string path = savename.substr(0,savename.find('/'));
+  //canv->Print(Form("%s/animation.gif+100",path.c_str()));
+  
+  delete canv;
+	
 }
 
 int main(int argc, char *argv[]){
@@ -504,87 +510,98 @@ int main(int argc, char *argv[]){
   lumi_7TeV  = "4.9 fb^{-1}";  // default is "5.1 fb^{-1}"
   lumi_sqrtS = "13 TeV";       // used with iPeriod = 0, e.g. for simulation-only plots (default is an empty string)
 
-	split(procs_,procString_,boost::is_any_of(","));
-	split(flashggCats_,flashggCatsStr_,boost::is_any_of(","));
-
-	if (isFlashgg_){
-		ncats_ =flashggCats_.size();
-		// Ensure that the loop over the categories does not go out of scope. 
-	 std::cout << "[INFO] consider "<< ncats_ <<" tags/categories" << std::endl;
-	}
-
-
-	gROOT->SetBatch();
-	gStyle->SetTextFont(42);
-
-
-	TFile *hggFile = TFile::Open(filename_.c_str());
-	RooWorkspace *hggWS;
-	hggWS = (RooWorkspace*)hggFile->Get(Form("wsig_%dTeV",sqrts_));
-
-	if (!hggWS) {
-		cerr << "Workspace is null" << endl;
-		exit(1);
-	}
-
-	RooRealVar *mass= (RooRealVar*)hggWS->var("CMS_hgg_mass");
-
-	RooRealVar *mh = (RooRealVar*)hggWS->var("MH");
-	mh->setVal(m_hyp_);
-	mass->setRange("higgsRange",m_hyp_-20.,m_hyp_+15.);
-
-	map<string,RooDataSet*> dataSets;
-	map<string,RooDataSet*> dataSetsGranular;
-	map<string,RooAddPdf*> pdfs;
-	map<string,RooAddPdf*> pdfsGranular;
-
-	if (isFlashgg_){
-		dataSets = getFlashggData(hggWS,ncats_,m_hyp_);
-		dataSetsGranular = getFlashggDataGranular(hggWS,ncats_,m_hyp_);
-		pdfs = getFlashggPdfs(hggWS,ncats_);
-		pdfsGranular = getFlashggPdfsGranular(hggWS,ncats_);
-	}
-	else {
-		dataSets = getGlobeData(hggWS,ncats_,m_hyp_);
-		pdfs = getGlobePdfs(hggWS,ncats_); 
-	}
-
-//	printInfo(dataSets,pdfs);
-
-	map<string,double> sigEffs;
-	map<string,double> fwhms;
-
-
-	system(Form("mkdir -p %s",outfilename_.c_str()));
-	system(Form("rm -f %s/animation.gif",outfilename_.c_str()));
-
-
-	for (map<string,RooDataSet*>::iterator dataIt=dataSets.begin(); dataIt!=dataSets.end(); dataIt++){
-		pair<double,double> thisSigRange = getEffSigma(mass,pdfs[dataIt->first],m_hyp_-10.,m_hyp_+10.);
-		//pair<double,double> thisSigRange = getEffSigBinned(mass,pdf[dataIt->first],m_hyp_-10.,m_hyp_+10);
-		vector<double> thisFWHMRange = getFWHM(mass,pdfs[dataIt->first],dataIt->second,m_hyp_-10.,m_hyp_+10.);
-		sigEffs.insert(pair<string,double>(dataIt->first,(thisSigRange.second-thisSigRange.first)/2.));
-		fwhms.insert(pair<string,double>(dataIt->first,thisFWHMRange[1]-thisFWHMRange[0]));
-		if (doCrossCheck_) performClosure(mass,pdfs[dataIt->first],dataIt->second,Form("%s/closure_%s.pdf",outfilename_.c_str(),dataIt->first.c_str()),m_hyp_-10.,m_hyp_+10.,thisSigRange.first,thisSigRange.second);
-		Plot(mass,dataIt->second,pdfs[dataIt->first],thisSigRange,thisFWHMRange,dataIt->first,Form("%s/%s",outfilename_.c_str(),dataIt->first.c_str()));
-	}
-	
+  split(procs_,procString_,boost::is_any_of(","));
+  split(flashggCats_,flashggCatsStr_,boost::is_any_of(","));
+  
+  if (isFlashgg_){
+    ncats_ =flashggCats_.size();
+    // Ensure that the loop over the categories does not go out of scope. 
+    std::cout << "[INFO] consider "<< ncats_ <<" tags/categories" << std::endl;
+  }
+  
+  
+  gROOT->SetBatch();
+  gStyle->SetTextFont(42);
+  
+  
+  TFile *hggFile = TFile::Open(filename_.c_str());
+  RooWorkspace *hggWS;
+  hggWS = (RooWorkspace*)hggFile->Get(Form("wsig_%dTeV",sqrts_));
+  if (!hggWS) {
+    cerr << "Workspace is null" << endl;
+    exit(1);
+  }
+  
+  RooRealVar *mass= (RooRealVar*)hggWS->var("CMS_hgg_mass");
+  std::cout<<"CMS_hgg_mass retrieved from ws"<<std::endl;
+  RooRealVar *mh = (RooRealVar*)hggWS->var("MH");
+  std::cout<<"MH retrieved from ws"<<std::endl;
+  mh->setVal(m_hyp_);
+  mass->setRange("higgsRange",m_hyp_-20.,m_hyp_+15.);
+  
+  map<string,RooDataSet*> dataSets;
+  map<string,RooDataSet*> dataSetsGranular;
+  map<string,RooAddPdf*> pdfs;
+  map<string,RooAddPdf*> pdfsGranular;
+  
+  if (isFlashgg_){
+    std::cout<<"isFlashgg_ if"<<std::endl;
+    dataSets = getFlashggData(hggWS,ncats_,m_hyp_);
+    dataSetsGranular = getFlashggDataGranular(hggWS,ncats_,m_hyp_);
+    pdfs = getFlashggPdfs(hggWS,ncats_);
+    pdfsGranular = getFlashggPdfsGranular(hggWS,ncats_);
+  }
+  else {
+    dataSets = getGlobeData(hggWS,ncats_,m_hyp_);
+    pdfs = getGlobePdfs(hggWS,ncats_); 
+  }
+  
+  
+  printInfo(dataSets,pdfs);
+  
+  map<string,double> sigEffs;
+  map<string,double> fwhms;
+  
+  
+  system(Form("mkdir -p %s",outfilename_.c_str()));
+  system(Form("rm -f %s/animation.gif",outfilename_.c_str()));
+  
+  
+  std::cout<<"datasets size "<<dataSets.size() <<std::endl;
+  std::cout<<"pdfs size "<<pdfs.size() <<std::endl;
+  
+  for (map<string,RooDataSet*>::iterator dataIt=dataSets.begin(); dataIt!=dataSets.end(); dataIt++){
+    std::cout<<"Iterating on datasets"<<std::endl;
+    std::cout<<"iterator first "<<dataIt->first<<" , second name "<<dataIt->second->GetName()<<std::endl;
+    std::cout<<"pdfs iterator first name "<<pdfs[dataIt->first]->GetName()<<std::endl;
+    pair<double,double> thisSigRange = getEffSigma(mass,pdfs[dataIt->first],m_hyp_-10.,m_hyp_+10.);
+    std::cout<<"got getEffSigma"<<std::endl;
+    //pair<double,double> thisSigRange = getEffSigBinned(mass,pdf[dataIt->first],m_hyp_-10.,m_hyp_+10);
+    vector<double> thisFWHMRange = getFWHM(mass,pdfs[dataIt->first],dataIt->second,m_hyp_-10.,m_hyp_+10.);
+    std::cout<<"got FWHM"<<std::endl;
+    sigEffs.insert(pair<string,double>(dataIt->first,(thisSigRange.second-thisSigRange.first)/2.));
+    fwhms.insert(pair<string,double>(dataIt->first,thisFWHMRange[1]-thisFWHMRange[0]));
+    if (doCrossCheck_) performClosure(mass,pdfs[dataIt->first],dataIt->second,Form("%s/closure_%s.pdf",outfilename_.c_str(),dataIt->first.c_str()),m_hyp_-10.,m_hyp_+10.,thisSigRange.first,thisSigRange.second);
+    Plot(mass,dataIt->second,pdfs[dataIt->first],thisSigRange,thisFWHMRange,dataIt->first,Form("%s/%s",outfilename_.c_str(),dataIt->first.c_str()));
+  }
+  
   for (map<string,RooDataSet*>::iterator dataIt=dataSetsGranular.begin(); dataIt!=dataSetsGranular.end(); dataIt++){
-		pair<double,double> thisSigRange = getEffSigma(mass,pdfsGranular[dataIt->first],m_hyp_-10.,m_hyp_+10.);
-		//pair<double,double> thisSigRange = getEffSigBinned(mass,pdf[dataIt->first],m_hyp_-10.,m_hyp_+10);
-		vector<double> thisFWHMRange = getFWHM(mass,pdfsGranular[dataIt->first],dataIt->second,m_hyp_-10.,m_hyp_+10.);
-		sigEffs.insert(pair<string,double>(dataIt->first,(thisSigRange.second-thisSigRange.first)/2.));
-		fwhms.insert(pair<string,double>(dataIt->first,thisFWHMRange[1]-thisFWHMRange[0]));
-		if (doCrossCheck_) performClosure(mass,pdfsGranular[dataIt->first],dataIt->second,Form("%s/closure_%s.pdf",outfilename_.c_str(),dataIt->first.c_str()),m_hyp_-10.,m_hyp_+10.,thisSigRange.first,thisSigRange.second);
-		Plot(mass,dataIt->second,pdfsGranular[dataIt->first],thisSigRange,thisFWHMRange,dataIt->first,Form("%s/%s",outfilename_.c_str(),dataIt->first.c_str()));
-	}
-
-	map<string,pair<double,double> > bkgVals;
-	map<string,vector<double> > sigVals;
-	map<string,pair<double,double> > datVals;
-	map<string,double> sobVals;
-
-	hggFile->Close();
-
+    std::cout<<"Iterating on granular datasets"<<std::endl;
+    pair<double,double> thisSigRange = getEffSigma(mass,pdfsGranular[dataIt->first],m_hyp_-10.,m_hyp_+10.);
+    //pair<double,double> thisSigRange = getEffSigBinned(mass,pdf[dataIt->first],m_hyp_-10.,m_hyp_+10);
+    vector<double> thisFWHMRange = getFWHM(mass,pdfsGranular[dataIt->first],dataIt->second,m_hyp_-10.,m_hyp_+10.);
+    sigEffs.insert(pair<string,double>(dataIt->first,(thisSigRange.second-thisSigRange.first)/2.));
+    fwhms.insert(pair<string,double>(dataIt->first,thisFWHMRange[1]-thisFWHMRange[0]));
+    if (doCrossCheck_) performClosure(mass,pdfsGranular[dataIt->first],dataIt->second,Form("%s/closure_%s.pdf",outfilename_.c_str(),dataIt->first.c_str()),m_hyp_-10.,m_hyp_+10.,thisSigRange.first,thisSigRange.second);
+    Plot(mass,dataIt->second,pdfsGranular[dataIt->first],thisSigRange,thisFWHMRange,dataIt->first,Form("%s/%s",outfilename_.c_str(),dataIt->first.c_str()));
+  }
+  
+  map<string,pair<double,double> > bkgVals;
+  map<string,vector<double> > sigVals;
+  map<string,pair<double,double> > datVals;
+  map<string,double> sobVals;
+  
+  hggFile->Close();
+  
 }
 
