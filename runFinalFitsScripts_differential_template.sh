531596c4842e27db00d600d709b90e97c155e55a
vtavolar <vittorio.tavolaro@cern.ch>
2018-04-24 14:32:15 +0200

    template of generic master script for fits

--- runFinalFitsScripts_differential_template.sh
+++ runFinalFitsScripts_differential_template.sh
@@ -11,7 +11,9 @@ usage(){
     echo "--procs, names of processes )" 
     echo "--cats, names of categories )" 
     echo "--range, range for POIs scan )" 
+    echo "--npoints, number of points for POIs scan )" 
     echo "--shiftOffDiag, correct for bias in off-diag elements when using diag replacement )" 
+    echo "--noSkip, do not skip datasets with conditions below min for signal model )" 
     echo "--refTagDiff, reference replacement tag for differentials )" 
     echo "--refTagWV, reference replacement tag for WV datasets )" 
     echo "--refProc, reference replacement process )" 
@@ -20,11 +22,16 @@ usage(){
     echo "--sigModOpt, more options to be propagated to signal model scripts )" 
     echo "--bkgModOpt, more options to be propagated to bkg model scripts )"     
     echo "--DatacardOpt, more options to be propagated to datacard scripts )"     
+    echo "--runCombineOnly, assume models and datacard are there and only run combine )" 
+    echo "--runDatacardOnly, only produce the datacard, assuming inputs are there )" 
+    echo "--runSignalOnly, only produce signal models )" 
+    echo "--runBackgroundOnly, only run bkg )" 
+    echo "--unblind, unblind mass plots )" 
 
 
 }
 
-if ! options=$(getopt -u -o hi:p:f: -l help,obs:,ext:,inputDir:,procs:,cats:,range:,shiftOffDiag:,refTagDiff:,refTagWV:,refProc:,refProcDiff:,refProcWV:,sigModOpt::,bkgModOpt::,DatacardOpt:: -- "$@")
+if ! options=$(getopt -u -o hi:p:f: -l help,obs:,ext:,inputDir:,procs:,cats:,range:,npoints:,shiftOffDiag:,noSkip:,refTagDiff:,refTagWV:,refProc:,refProcDiff:,refProcWV:,sigModOpt::,bkgModOpt::,DatacardOpt::,runCombineOnly:,runDatacardOnly:,runSignalOnly:,runBackgroundOnly:,unblind: -- "$@")
 then
 # something went wrong, getopt will put out an error message for us
     exit 1
@@ -36,8 +43,10 @@ EXT="ext"
 INPUTPATH="input/path/"
 PROCS="ggh"
 CATS="cat0"
-range="[1,-1.0,3.0]"
+RANGE="[1,-1.0,3.0]"
+NPOINTS="20"
 SHIFTOFFDIAG=1
+NOSKIP=0
 REFTAGDIFF="cat0"
 REFTAGWV="cat0"
 REFPROC="ggh"
@@ -46,6 +55,11 @@ REFPROCWV="ggh"
 SIGMODOPT=""
 BKGMODOPT=""
 DATACARDOPT=""
+COMBINEONLY=0
+DATACARDONLY=0
+SIGNALONLY=0
+BKGONLY=0
+UNBLIND=0
 
 while [ $# -gt 0 ]
 do
@@ -57,7 +71,9 @@ do
 	--procs) PROCS=$2; shift;;
 	--cats) CATS=$2; shift;;
 	--range) RANGE=$2; shift;;
+	--npoints) NPOINTS=$2; shift;;
 	--shiftOffDiag) SHIFTOFFDIAG=$2; shift;;
+	--noSkip) NOSKIP=$2; shift;;
 	--refTagDiff) REFTAGDIFF=$2; shift;;
 	--refTagWV) REFTAGWV=$2; shift;;
 	--refProc) REFPROC=$2; shift;;
@@ -66,6 +82,11 @@ do
 	--sigModOpt) SIGMODOPT=$2; shift;;
 	--bkgModOpt) BKGMODOPT=$2; shift;;
 	--DatacardOpt) DATACARDOPT=$2; shift;;
+	--runCombineOnly) COMBINEONLY=$2; shift;;
+	--runDatacardOnly) DATACARDONLY=$2; shift;;
+	--runSignalOnly) SIGNALONLY=$2; shift;;
+	--runBackgroundOnly) BKGONLY=$2; shift;;
+	--unblind) UNBLIND=$2; shift;;
 
 	(--) shift; break;;
 	(-*) usage; echo "$0: error - unrecognized option $1" 1>&2; usage >> /dev/stderr; exit 1;;
@@ -73,6 +94,42 @@ do
     esac
     shift
 done
+
+DOSIG=1
+DOBKG=1
+DODATACARD=1
+DOCOMBINE=1
+
+if [ $SIGNALONLY -eq 1 ]; then
+DOSIG=1
+DOBKG=0
+DODATACARD=0
+DOCOMBINE=0
+fi
+
+
+if [ $BKGONLY -eq 1 ]; then
+DOSIG=0
+DOBKG=1
+DODATACARD=0
+DOCOMBINE=0
+fi
+
+
+if [ $COMBINEONLY -eq 1 ]; then
+DOSIG=0
+DOBKG=0
+DODATACARD=0
+DOCOMBINE=1
+fi
+
+
+if [ $DATACARDONLY -eq 1 ]; then
+DOSIG=0
+DOBKG=0
+DODATACARD=1
+DOCOMBINE=0
+fi
 
 
 
@@ -123,33 +180,59 @@ if [ $SHIFTOFFDIAG == 1 ]; then
     SIGMODOPT="${SIGMODOPT} --shiftOffDiag"
 fi
 
+if [ $NOSKIP == 1 ]; then
+    SIGMODOPT="${SIGMODOPT} --noSkip"
+fi
+
 ##signal model preparation
 #The first tiem you run this command, it will run the signal f-test to determine the number of gaussians to use for each tag/process. You'll be prompted to use the output of this to fill in the required config file. Then re-run to build the signal model.
 ##./runFinalFitsScripts.sh -i $FILE -p $PROCS -f $CATS --ext $EXT --batch $BATCH --intLumi $INTLUMI --smears $SMEARS --scales $SCALES --signalOnly --bs 3.5 --shiftOffDiag --refTagDiff $5 --refTagWV $6 --refProcWV $7 --refProcDiff $8 --refProc $9 ## --normalisationCut "processIndex==11"
-######./runFinalFitsScripts.sh -i $FILE -p $PROCS -f $CATS --ext $EXT --batch $BATCH --intLumi $INTLUMI --smears $SMEARS --scales $SCALES --signalOnly --bs 3.5 --refTagDiff $REFTAGDIFF --refTagWV $REFTAGWV --refProcWV $REFPROCWV --refProcDiff $REFPROCDIFF --refProc $REFPROC $SIGMODOPT ## --normalisationCut "processIndex==11"
 
-##background model preparations
-#by default this produces blinded plots... use option --unblind to unblind.
-#############./runFinalFitsScripts.sh -i $FILE -p $PROCS -f $CATS --ext $EXT --intLumi $INTLUMI --backgroundOnly --dataFile $DATA --isData --batch $BATCH $BKGMODOPT ##--noBkgPlots OR --monitorDataPlots
-#####./runFinalFitsScripts.sh -i $FILE -p $PROCS -f $CATS --ext $EXT --intLumi $INTLUMI --backgroundOnly --dataFile $DATA --isData --batch $BATCH  --unblind
+if [ $DOSIG -eq 1 ]; then
+    ./runFinalFitsScripts.sh -i $FILE -p $PROCS -f $CATS --ext $EXT --batch $BATCH --intLumi $INTLUMI --smears $SMEARS --scales $SCALES --signalOnly --bs 3.5 --refTagDiff $REFTAGDIFF --refTagWV $REFTAGWV --refProcWV $REFPROCWV --refProcDiff $REFPROCDIFF --refProc $REFPROC $SIGMODOPT ## --normalisationCut "processIndex==11"
+fi
 
+if [ $DOBKG -eq 1 ]; then
+    ##background model preparations
+    #by default this produces blinded plots... use option --unblind to unblind.
+    if [ $UNBLIND -eq 0 ]; then
+	./runFinalFitsScripts.sh -i $FILE -p $PROCS -f $CATS --ext $EXT --intLumi $INTLUMI --backgroundOnly --dataFile $DATA --isData --batch $BATCH $BKGMODOPT ##--noBkgPlots OR --monitorDataPlots
+    else
+    ./runFinalFitsScripts.sh -i $FILE -p $PROCS -f $CATS --ext $EXT --intLumi $INTLUMI --backgroundOnly --dataFile $DATA --isData --batch $BATCH  --unblind
+    fi
+fi
 
-##making the datacard
-# this step generates the datacard using the signal model as input.
-./runFinalFitsScripts.sh -i $FILE125_IAOA -p $PROCS -f $CATS --ext $EXT  --intLumi $INTLUMI --datacardOnly --dataFile $DATA --isData --batch $BATCH --datacardDifferential --multiPdf $DATACARDOPT ##--noSysts
+if [ $DODATACARD -eq 1 ]; then
+    ##making the datacard
+    # this step generates the datacard using the signal model as input.
+    ./runFinalFitsScripts.sh -i $FILE125_IAOA -p $PROCS -f $CATS --ext $EXT  --intLumi $INTLUMI --datacardOnly --dataFile $DATA --isData --batch $BATCH --datacardDifferential --multiPdf $DATACARDOPT ##--noSysts
+fi
 
 MYPWD=${PWD}
 cd /mnt/t3nfs01/data01/shome/vtavolar/combine74/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/AfterMoriond17
 mkdir ${EXT}
-cp ${MYPWD}/Signal/outdir_${EXT}/CMS-HGG_sigfit_${EXT}_*root ${EXT}
-cp ${MYPWD}/Background/CMS-HGG_multipdf_${EXT}.root ${EXT}
-cp ${MYPWD}/Datacard/Datacard_13TeV_${EXT}.txt ${EXT}
-cd ${EXT}
-python ../mergeWorkspaces.py  CMS-HGG_sigfit_${EXT}.root CMS-HGG_sigfit_${EXT}_*root
-set -x
-sed -i "s/CMS-HGG_sigfit_${EXT}_.*root/CMS-HGG_sigfit_${EXT}.root/" Datacard_13TeV_${EXT}.txt
-sed -i "s/CMS-HGG_${EXT}_13TeV_multipdf.root/CMS-HGG_multipdf_${EXT}.root/" Datacard_13TeV_${EXT}.txt
-
 POIS=${PROCS%",OutsideAcceptance"}
-cd ..
-bash multi_signal_model_MjjEta2p5_template.sh ${EXT} Datacard_13TeV_${EXT}.txt ${POIS} ${RANGE} | tee ${EXT}/multi_signal_model.log
+if [ $DOCOMBINE -eq 1 ] || [ $DOSIG -eq 1 ]; then
+    cp ${MYPWD}/Signal/outdir_${EXT}/CMS-HGG_sigfit_${EXT}_*root ${EXT}
+    cp ${MYPWD}/Background/CMS-HGG_multipdf_${EXT}.root ${EXT}
+    cp ${MYPWD}/Datacard/Datacard_13TeV_${EXT}.txt ${EXT}
+    cd ${EXT}
+    rm CMS-HGG_sigfit_${EXT}.root
+    python ../mergeWorkspaces.py  CMS-HGG_sigfit_${EXT}.root CMS-HGG_sigfit_${EXT}_*root
+    set -x
+    sed -i "s/CMS-HGG_sigfit_${EXT}_.*root/CMS-HGG_sigfit_${EXT}.root/" Datacard_13TeV_${EXT}.txt
+    sed -i "s/CMS-HGG_${EXT}_13TeV_multipdf.root/CMS-HGG_multipdf_${EXT}.root/" Datacard_13TeV_${EXT}.txt
+    cd ..
+fi
+
+if [ $DODATACARD -eq 1 ]; then
+    cp ${MYPWD}/Datacard/Datacard_13TeV_${EXT}.txt ${EXT}
+    cd ${EXT}
+    sed -i "s/CMS-HGG_sigfit_${EXT}_.*root/CMS-HGG_sigfit_${EXT}.root/" Datacard_13TeV_${EXT}.txt
+    sed -i "s/CMS-HGG_${EXT}_13TeV_multipdf.root/CMS-HGG_multipdf_${EXT}.root/" Datacard_13TeV_${EXT}.txt
+    cd ..
+fi
+
+if [ $DOCOMBINE -eq 1 ]; then
+    bash multi_signal_model_MjjEta2p5_template.sh ${EXT} Datacard_13TeV_${EXT}.txt ${POIS} ${RANGE} ${NPOINTS} | tee ${EXT}/multi_signal_model.log
+fi
